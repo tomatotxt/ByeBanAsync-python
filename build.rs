f9b40ba0fd1df0f0d3ad952a1a4577c49c17213a
tomato.txt <104869883+tomatotxt@users.noreply.github.com>
2025-12-11 10:15:41 -0500

    Delete build.rs

--- build.rs
+++
@@ -1,7 +0,0 @@
-use embed_manifest::{embed_manifest_file};
-
-fn main() {
-    if std::env::var_os("CARGO_CFG_WINDOWS").is_some() {
-        embed_manifest_file("src/app.manifest").expect("unable to embed manifest file");
-    }
-}
